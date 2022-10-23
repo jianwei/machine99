154fc2873df900239805c48f01e937979c7a069c
Carlos  chen <ccjjww1222@gmail.com>
2022-10-23 21:23:01 +0800

    Update detect.py

--- detect/RKNN/detect.py
+++ detect/RKNN/detect.py
@@ -49,8 +49,8 @@ class AutoScale:
         return self.__new_img
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+# def sigmoid(x):
+#     return 1 / (1 + np.exp(-x))
 
 
 # def filter_boxes(boxes, box_confidences, box_class_probs, conf_thres):
@@ -96,17 +96,17 @@ def nms_boxes(boxes, scores, iou_thres):
     return keep
 
 
-def plot_one_box(x, img, color=None, label=None, line_thickness=None):
-    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-    color = color or [random.randint(0, 255) for _ in range(3)]
-    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-    if label:
-        tf = max(tl - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+# def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+#     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+#     color = color or [random.randint(0, 255) for _ in range(3)]
+#     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+#     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+#     if label:
+#         tf = max(tl - 1, 1)  # font thickness
+#         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+#         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+#         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+#         cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
 def letterbox(img, new_wh=(416, 416), color=(114, 114, 114)):
@@ -177,6 +177,7 @@ def yolov5_post_process(input_data):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
 
 
 def xywh2xyxy(x):
