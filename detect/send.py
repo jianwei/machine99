389535b3d35daf43f28d44c5d7567716baf7f7d8
Carlos  chen <ccjjww1222@gmail.com>
2022-10-26 17:23:31 +0800

    1

--- detect/send.py
+++ detect/send.py
@@ -1,7 +1,17 @@
 from utils.unix_socket import unix_socket
 import json
 
-u = unix_socket("../uds_socket")
+import yaml
+
+def get_yaml_data(config_yaml):
+    with open(config_yaml, encoding='utf-8')as file:
+        content = file.read()
+        data = yaml.load(content, Loader=yaml.FullLoader)
+        return data
+
+# u = unix_socket("../uds_socket")
+unix_socket_path = get_yaml_data('../config.yaml').get('unix_socket')
+u = unix_socket(unix_socket_path)
 message = json.dumps({"a":1})
 u.send_message(message)
 
