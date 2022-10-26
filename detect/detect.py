07762a2862217621fb9e153743a828e916d6a227
Carlos  chen <ccjjww1222@gmail.com>
2022-10-26 14:32:40 +0800

    Update detect.py

--- detect/detect.py
+++ detect/detect.py
@@ -5,9 +5,6 @@ import argparse
 
 
 def main(camera_id):
-
-    print(camera_id,type(camera_id))
-
     filt_folder = os.getcwd()
     RKNN_MODEL_PATH = filt_folder + "/weights/box.rknn"
     detector = RKNNDetector(RKNN_MODEL_PATH)
@@ -17,15 +14,16 @@ def main(camera_id):
         print("--------------------------------------------------------------------------------------------------")
         t0 = time.time()
         success,img=cap.read()
-        src_h, src_w = img.shape[:2]
-        img_1 = detector.predict(img)
-        t1 = time.time()
-        fps = round(1/(t1-t0),3)
-        cv2.putText(img_1,"fps:{}".format(fps), (0,30),0,1,(0, 0, 255),thickness=2,lineType=cv2.LINE_AA)
-        print("width:{},height:{},fps:{}".format(src_w,src_h,fps) )
-        cv2.imshow("Video",img_1)
-        if cv2.waitKey(1)&0xFF==ord('q'):
-            break
+        if success:
+            src_h, src_w = img.shape[:2]
+            img_1 = detector.predict(img)
+            t1 = time.time()
+            fps = round(1/(t1-t0),3)
+            cv2.putText(img_1,"fps:{}".format(fps), (0,30),0,1,(0, 0, 255),thickness=2,lineType=cv2.LINE_AA)
+            print("width:{},height:{},fps:{}".format(src_w,src_h,fps) )
+            cv2.imshow("Video",img_1)
+            if cv2.waitKey(1)&0xFF==ord('q'):
+                break
 
 
 
