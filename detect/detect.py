2764f20f3543a70192045639abf306dbe1ce1f78
Carlos  chen <ccjjww1222@gmail.com>
2022-10-26 14:57:04 +0800

    Update detect.py

--- detect/detect.py
+++ detect/detect.py
@@ -15,15 +15,15 @@ def main(camera_id,save_video=False):
         now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         #对视频设置的编码解码的方式MPEG-4编码
         fource=cv2.VideoWriter_fourcc(*'DIVX')
-        # source_video=cv2.VideoWriter('./run/source/{}.mp4'.format(now_time),fource,20,(640,480))
-        inference_video=cv2.VideoWriter('./run/inference/{}.mp4'.format(now_time),fource,20,(640,480))
+        source_video=cv2.VideoWriter('./run/source/{}.mp4'.format(now_time),fource,20,(640,480))
+        # inference_video=cv2.VideoWriter('./run/inference/{}.mp4'.format(now_time),fource,20,(640,480))
 
     while True:
         print("--------------------------------------------------------------------------------------------------")
         t0 = time.time()
         success,img=cap.read()
-        # if save_video:
-        #     source_video.write(img)
+        if save_video:
+            source_video.write(img)
         if success:
             src_h, src_w = img.shape[:2]
             img_1 = detector.predict(img)
@@ -31,9 +31,6 @@ def main(camera_id,save_video=False):
             fps = round(1/(t1-t0),3)
             cv2.putText(img_1,"fps:{}".format(fps), (0,30),0,1,(0, 0, 255),thickness=2,lineType=cv2.LINE_AA)
             print("width:{},height:{},fps:{}".format(src_w,src_h,fps) )
-            if save_video:
-                print(123)
-                inference_video.write(img_1)
             cv2.imshow("3588_inference_video",img_1)
             if cv2.waitKey(1)&0xFF==ord('q'):
                 break
