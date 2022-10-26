74678f6dd3cbbdebe57b432160c3edc762ccc140
Carlos  chen <ccjjww1222@gmail.com>
2022-10-26 17:27:26 +0800

    Update send.py

--- detect/send.py
+++ detect/send.py
@@ -1,7 +1,8 @@
 from utils.unix_socket import unix_socket
 import json
-
 import yaml
+import time
+
 
 def get_yaml_data(config_yaml):
     with open(config_yaml, encoding='utf-8')as file:
@@ -9,10 +10,11 @@ def get_yaml_data(config_yaml):
         data = yaml.load(content, Loader=yaml.FullLoader)
         return data
 
+
 # u = unix_socket("../uds_socket")
 unix_socket_path = get_yaml_data('../config.yaml').get('unix_socket')
 u = unix_socket(unix_socket_path)
 for i in range(100):
-    message = json.dumps({"a":i})
+    message = json.dumps({"a": i})
     u.send_message(message)
-
+    time.sleep(1)
