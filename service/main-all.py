78e3b9fb90930d847b646d37f58ddfb8121aab05
Carlos  chen <ccjjww1222@gmail.com>
2022-11-08 13:14:26 +0800

    Create main-all.py

---
+++ service/main-all.py
@@ -0,0 +1,15 @@
+import os
+import threading
+
+def main(cmds):
+    for cmd in cmds:
+        thread = threading.Thread(target=run_cmd, args=(cmd,))
+        thread.start()
+
+def run_cmd(cmd):
+    print("cmd:",cmd)
+    os.system(cmd)
+
+if __name__ == '__main__':
+    cmds = ["ython3 main.py --to_do run","python3 main.py --to_do work"]
+    main(cmds)
