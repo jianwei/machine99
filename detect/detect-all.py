42da038e30f8d2828f2f4d1e5af0e30a33672613
Carlos  chen <ccjjww1222@gmail.com>
2022-11-08 15:30:48 +0800

    Update detect-all.py

--- detect/detect-all.py
+++ detect/detect-all.py
@@ -1,18 +1,25 @@
 import numpy as np
-import cv2
 import multiprocessing
+from utils.rknn import RKNNDetector
+import cv2
 import time
- 
+import os
+import argparse
 
- 
+
+def main(camera_id):
+    video_read(camera_id)
+    pass
+
+
 def video_read(camera_id):
     cap = cv2.VideoCapture(camera_id)
     width = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
     height = (int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    print(width,height)
+    print(camera_id, width, height)
     while (cap.isOpened()):
-        ret,frame = cap.read()
-        cv2.imshow('camera', frame )
+        ret, frame = cap.read()
+        cv2.imshow('camera', frame)
         key = cv2.waitKey(10)
         if int(key) == 113:
             break
@@ -21,8 +28,8 @@ def video_read(camera_id):
 
 if __name__ == '__main__':
     print("主进程开始启动！")
-    camera_arr = [20,22]
+    camera_arr = [20, 22]
     for item in camera_arr:
-        p = multiprocessing.Process(target = video_read, args = (item,))
+        p = multiprocessing.Process(target=main, args=(item,))
         p.start()
     print('程序结束！')
