8cb818a8bffdd346033311dbaaaa3573c7200f58
Carlos  chen <ccjjww1222@gmail.com>
2022-11-08 14:17:55 +0800

    Create camera.py

---
+++ detect/camera.py
@@ -0,0 +1,20 @@
+import cv2
+import threading
+
+def main(camera_arr):
+    for camera_id in camera_arr:
+        thread = threading.Thread(target=run_cmd, args=(camera_id,))
+        thread.start()
+
+
+def run_cmd(camera_id):
+    cap=cv2.VideoCapture(camera_id) #cv2.VideoCapture(0)代表调取摄像头资源，其中0代表电脑摄像头，1代表外接摄像头(usb摄像头)
+    while True:
+        success,img=cap.read()
+        cv2.imshow("Video",img)
+        if cv2.waitKey(1)&0xFF==ord('q'):
+            break
+
+if __name__ == '__main__':
+    camera_arr = [20,22]
+    main(camera_arr)
