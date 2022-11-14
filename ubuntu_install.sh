b924a682df400c92f613d18c1555e037e76a26ab
Carlos  chen <ccjjww1222@gmail.com>
2022-11-14 14:01:12 +0800

    1

---
+++ ubuntu_install.sh
@@ -0,0 +1,4 @@
+# sudo 权限   先安装python3.9 再安装 pip3
+sudo apt update;
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev;
+sudo wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz
