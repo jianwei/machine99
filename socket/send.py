5b056088c4b13951512e65e79f8e69d81a44b778
Carlos  chen <ccjjww1222@gmail.com>
2022-11-17 16:03:21 +0800

    1

--- socket/send.py
+++ socket/send.py
@@ -13,11 +13,7 @@ def get_yaml_data(config_yaml):
         return data
 
 
-# u = unix_socket("../uds_socket")
 server_address = get_yaml_data('../config.yaml').get("serial_control").get('unix_socket')
-
-
-# server_address = './uds_socket'
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 try:
     sock.connect(server_address)
@@ -25,12 +21,8 @@ except socket.error as msg:
     print(msg)
     sys.exit(1)
 
-# message = json.dumps({"uuid": })
-# sock.send(b'{"": 1}')
-# sock.close()
-# u = unix_socket(unix_socket_path)
+
 for i in range(100):
     message = json.dumps({"uuid": i,"cmd":"TR {}".format(i+10)})
-    # u.send_message(message)
     sock.send(message.encode("UTF-8"))
     time.sleep(1)
--- socket/utils/serial_control.py
+++ socket/utils/serial_control.py
@@ -36,9 +36,9 @@ class serial_control():
         uuid = message["uuid"]
         print("cmd:",cmd)
         if (cmd):
-            # print("cmd:{},begin_time:{}".format(cmd, time.time()))
+            print("cmd:{},begin_time:{}".format(cmd, time.time()))
             self.ser.write(cmd.encode())
-            # print("cmd:end write:{}".format(time.time()))
+            print("cmd:end write:{}".format(time.time()))
             try:
                 cnt = 1
                 ret_all = ""
@@ -54,7 +54,7 @@ class serial_control():
                         response_arr = ret_all.splitlines()
                         ret = response_arr[len(
                             response_arr)-1] if len(response_arr) > 0 else ""
-                        # print("1--cnt:{},send_cmd:uuid:{},cmd:{},ret:{},difftime:{},response:{}".format( cnt, uuid, cmd, ret, diff, ret_all))
+                        print("1--cnt:{},send_cmd:uuid:{},cmd:{},ret:{},difftime:{},response:{}".format( cnt, uuid, cmd, ret, diff, ret_all))
                         time.sleep(0.1)
                         s1 = re.compile('^(-?[1-9]|0{1}\d*)$')
                         r1 = s1.findall(ret)
