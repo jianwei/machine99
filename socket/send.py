1c1d9459f3165493c538f3f85ba2e224e489f329
Carlos  chen <ccjjww1222@gmail.com>
2022-11-17 15:49:58 +0800

    Create send.py

---
+++ socket/send.py
@@ -0,0 +1,20 @@
+from utils.unix_socket import unix_socket
+import json
+import yaml
+import time
+
+
+def get_yaml_data(config_yaml):
+    with open(config_yaml, encoding='utf-8')as file:
+        content = file.read()
+        data = yaml.load(content, Loader=yaml.FullLoader)
+        return data
+
+
+# u = unix_socket("../uds_socket")
+unix_socket_path = get_yaml_data('../config.yaml').get("serial_control").get('unix_socket')
+u = unix_socket(unix_socket_path)
+for i in range(100):
+    message = json.dumps({"a": i})
+    u.send_message(message)
+    time.sleep(1)
