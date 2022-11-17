4962eff2f534a57b443c4499e4ac67bc2273f1f3
Carlos  chen <ccjjww1222@gmail.com>
2022-11-17 13:42:54 +0800

    1
    2

--- service/main-all.py
+++ service/main-all.py
@@ -1,8 +1,20 @@
 import os
 import threading
+import yaml
 
-def main(cmds):
-    for cmd in cmds:
+
+def get_yaml_data(config_yaml):
+    with open(config_yaml, encoding='utf-8') as file:
+        content = file.read()
+        data = yaml.load(content, Loader=yaml.FullLoader)
+        return data
+
+def main():
+    yaml_config = get_yaml_data('../config.yaml')
+    cameras = yaml_config.get("camera")
+    for item in cameras:
+        cmd = "python3 main.py --to_do {}".format(item.get("to_do"))
+        print(cmd)
         thread = threading.Thread(target=run_cmd, args=(cmd,))
         thread.start()
 
@@ -11,5 +23,5 @@ def run_cmd(cmd):
     os.system(cmd)
 
 if __name__ == '__main__':
-    cmds = ["python3 main.py --to_do run1","python3 main.py --to_do run2","python3 main.py --to_do work"]
-    main(cmds)
+    # cmds = ["python3 main.py --to_do run1","python3 main.py --to_do run2","python3 main.py --to_do work"]
+    main()
