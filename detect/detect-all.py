597906785cdccf27ac8439ac1dc0ee6cea8817ef
Carlos  chen <ccjjww1222@gmail.com>
2022-11-17 15:29:02 +0800

    Update detect-all.py

--- detect/detect-all.py
+++ detect/detect-all.py
@@ -1,8 +1,11 @@
-import os
+import os,yaml
 import threading
 
-def main(cmds):
-    for cmd in cmds:
+def main():
+    config = get_yaml_data("../config.yaml")
+    cameras = config.get("camera")
+    for item in cameras:
+        cmd = "python3 detect.py --camera_id {} --to_do {}".format(item.get("camera_id"),item.get("to_do"))
         thread = threading.Thread(target=run_cmd, args=(cmd,))
         thread.start()
 
@@ -10,24 +13,30 @@ def run_cmd(cmd):
     print("cmd:",cmd)
     os.system(cmd)
 
-if __name__ == '__main__':
+def get_yaml_data(config_yaml):
+    with open(config_yaml, encoding='utf-8') as file:
+        content = file.read()
+        data = yaml.load(content, Loader=yaml.FullLoader)
+        return data
 
+if __name__ == '__main__':
+    main()
     # cmds = ["python3 detect.py --camera_id 0 --to_do work","python3 detect.py --camera_id 2 --to_do run"]
-    cmd_arr = [
-        {
-            "camera_id":"0",
-            "to_do":"run1",
-        },{
-            "camera_id":"2",
-            "to_do":"run2",
-        },{
-            "camera_id":"4",
-            "to_do":"work",
-        }
-    ]
-    cmds = []
-    for item in cmd_arr:
-        cmd = "python3 detect.py --camera_id {} --to_do {}".format(item.get("camera_id"),item.get("to_do"))
-        cmds.append(cmd)
-    # cmds = ["python3 detect.py --camera_id 0 --to_do work","python3 detect.py --camera_id 2 --to_do run"]
-    main(cmds)
+    # cmd_arr = [
+    #     {
+    #         "camera_id":"0",
+    #         "to_do":"run1",
+    #     },{
+    #         "camera_id":"2",
+    #         "to_do":"run2",
+    #     },{
+    #         "camera_id":"4",
+    #         "to_do":"work",
+    #     }
+    # ]
+    # cmds = []
+    # for item in cmd_arr:
+    #     cmd = "python3 detect.py --camera_id {} --to_do {}".format(item.get("camera_id"),item.get("to_do"))
+    #     cmds.append(cmd)
+    # # cmds = ["python3 detect.py --camera_id 0 --to_do work","python3 detect.py --camera_id 2 --to_do run"]
+    # main(cmds)
