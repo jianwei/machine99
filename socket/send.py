fe95365db1825ebbbf6eec3f014e24a82d58f5a0
Carlos  chen <ccjjww1222@gmail.com>
2022-11-18 10:14:10 +0800

    1
    2

--- socket/send.py
+++ socket/send.py
@@ -14,16 +14,22 @@ def get_yaml_data(config_yaml):
 
 
 server_address = get_yaml_data('../config.yaml').get("serial_control").get('unix_socket')
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-try:
-    sock.connect(server_address)
-except socket.error as msg:
-    print(msg)
-    sys.exit(1)
+# sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+# try:
+#     sock.connect(server_address)
+# except socket.error as msg:
+#     print(msg)
+#     sys.exit(1)
 
 
-for i in range(100):
+for i in range(30):
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    try:
+        sock.connect(server_address)
+    except socket.error as msg:
+        print(msg)
     message = json.dumps({"uuid": i,"cmd":"TR {}".format(i+10)})
     print(message)
     sock.send(message.encode("UTF-8"))
+    sock.close()
     time.sleep(1)
