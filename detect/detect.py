50865a0eddd1b823549f8dc6e5f66e3f7af26b12
Carlos  chen <ccjjww1222@gmail.com>
2022-11-22 14:57:26 +0800

    Update detect.py

--- detect/detect.py
+++ detect/detect.py
@@ -35,6 +35,7 @@ def main(camera_id,save_video=False,to_do="run"):
             next_data_reasult = detector.next_data_ret
             print("next_data_ret:",next_data_reasult)
             debug_draw_line(next_data_reasult,img_1)
+            debug_NPU_load(img_1,src_w,src_h)
 
             avg_inference_time = detector.get_inference_time()
             avg_yolo_time = detector.get_yolo_time()
@@ -67,6 +68,12 @@ def main(camera_id,save_video=False,to_do="run"):
                 if save_video:
                     print("save video to:{},inference_path:{}".format(video_path,inference_path))
                 break
+
+
+def debug_NPU_load(img_1,src_w,src_h):
+    tmp_cmd = 'sudo cat /sys/kernel/debug/rknpu/load'
+    val = os.popen(tmp_cmd)
+    cv2.putText(img_1,"{}".format(val), int(src_w/2-20,src_h-10),0,0.6,(0, 255, 255),thickness=2,lineType=cv2.LINE_AA)
 
 def debug_draw_line(ret,img_1):
     lines  = json.loads(ret).get("reasult").get("lines_format")
