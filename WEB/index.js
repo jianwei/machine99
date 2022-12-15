bf5b370e2984372f64837ccc6f5de7b693246bba
Carlos  chen <ccjjww1222@gmail.com>
2022-12-15 16:16:29 +0800

    1
    2

--- WEB/index.js
+++ WEB/index.js
@@ -1,5 +1,32 @@
 const express = require('express');
 const app = express();
-app.listen(1080, () => {
+const ejs = require('ejs');
+const YAML = require('yamljs');
+const fs = require("fs");
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+
+app.get("/",(req,res)=>{
+    res.render('index.html', { title: 'DisCloudDisk' });
+})
+
+
+app.get("/setting",(req,res)=>{
+    console.log("setting:",req.query.data)
+    data = req.query.data;
+    data = JSON.parse(atob(data))
+    res.send(data);
+})
+
+app.get("/load",(req,res)=>{
+    console.log("setting  request")
+    var data = YAML.parse(fs.readFileSync("../config.yaml").toString());
+    res.send(data);
+})
+
+
+app.listen(81, () => {
     console.log('running at http://127.0.0.1');
 })
+
+
